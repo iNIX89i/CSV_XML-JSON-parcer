5c94c079355a4f31593ea685de4dde608b14dd26
Nikita B <niko.nix@mail.ru>
2022-05-27 00:04:14 +0400

    JSON парсер v.1

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,137 @@
+package ru.netology;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Type;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String fileNameJson = "data.json";
+        String json = listToJson(list);
+        writeFile(json, fileNameJson);
+        String fileNameJson2 = "data2.json";
+        List<Employee> list2 = parseXML("data.xml");
+        String json2 = listToJson(list2);
+        writeFile(json2, fileNameJson2);
+    }
+
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee> list = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            list = csv.parse();
+            list.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        String json = gson.toJson(list, listType);
+        System.out.println(gson.toJson(list));
+        return json;
+    }
+
+    public static String writeFile(String json, String fileNameJson) {
+        try (FileWriter file = new FileWriter(fileNameJson)) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+
+    public static List<Employee> parseXML(String fileName) throws IOException, SAXException, ParserConfigurationException {
+        List<Employee> list = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element " + doc.getDocumentElement().getNodeName());
+            NodeList nodeLst = doc.getElementsByTagName("employee");
+            System.out.println("Information of all employees");
+
+            for (int s = 0; s < nodeLst.getLength(); s++) {
+                Node fstNode = nodeLst.item(s);
+                if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element fstElmnt = (Element) fstNode;
+                    NodeList fstNmElmntLst = fstElmnt.getElementsByTagName("id");
+                    Element fstNmElmnt = (Element) fstNmElmntLst.item(0);
+                    NodeList fstNm = fstNmElmnt.getChildNodes();
+                    String ID = ((Node) fstNm.item(0)).getNodeValue();
+                    System.out.println("id : " + ID);
+
+                    NodeList lstNmElmntLst2 = fstElmnt.getElementsByTagName("firstName");
+                    Element lstNmElmnt2 = (Element) lstNmElmntLst2.item(0);
+                    NodeList lstNm2 = lstNmElmnt2.getChildNodes();
+                    String FirstName = ((Node) lstNm2.item(0)).getNodeValue();
+                    System.out.println("firstName : " + FirstName);
+
+                    NodeList lstNmElmntLst3 = fstElmnt.getElementsByTagName("lastName");
+                    Element lstNmElmnt3 = (Element) lstNmElmntLst3.item(0);
+                    NodeList lstNm3 = lstNmElmnt3.getChildNodes();
+                    String LastName = ((Node) lstNm3.item(0)).getNodeValue();
+                    System.out.println("lastName : " + LastName);
+
+                    NodeList lstNmElmntLst4 = fstElmnt.getElementsByTagName("country");
+                    Element lstNmElmnt4 = (Element) lstNmElmntLst4.item(0);
+                    NodeList lstNm4 = lstNmElmnt4.getChildNodes();
+                    String Country = ((Node) lstNm4.item(0)).getNodeValue();
+                    System.out.println("country : " + Country);
+
+                    NodeList lstNmElmntLst5 = fstElmnt.getElementsByTagName("age");
+                    Element lstNmElmnt5 = (Element) lstNmElmntLst5.item(0);
+                    NodeList lstNm5 = lstNmElmnt5.getChildNodes();
+                    String Age = ((Node) lstNm5.item(0)).getNodeValue();
+                    System.out.println("age : " + Age);
+
+                    Employee employee = new Employee(Long.parseLong(ID), FirstName, LastName, Country, Integer.parseInt(Age));
+                    list.add(employee);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
